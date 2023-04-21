b42a222aade89cd13e056b05d5819baf7da68ffe
Sophie <sophieremery@gmail.com>
2023-04-21 14:06:47 +0100

    test2

---
+++ selection/selection_part1.py
@@ -0,0 +1,11 @@
+person = input("How old are you?  ")
+
+if person >= 18:
+    print("You are in category B")
+
+if person >= 16:
+    print("You are in category B")
+
+if person < 16:
+    print("You are in category C")
+    
