41a2b18f111c47131747bb729144fad0c3c237b8
Sophie <sophieremery@gmail.com>
2023-04-21 14:15:42 +0100

    part 1

---
+++ selection_part1.py
@@ -0,0 +1,17 @@
+person = int(input("How old are you?  "))
+
+#if person >= 18:
+    #print("You are in category A")
+
+#if person >= 16:
+    #print("You are in category B")
+
+#if person < 16:
+    #print("You are in category C")
+
+if person >= 18:
+    print("You are in category A")
+elif person >= 16:
+    print("You are in category B")
+else:
+    print("You are in category C")    
